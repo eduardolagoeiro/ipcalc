46272eaee42b8c0afd610319585a47ff568bf0e3
Eduardo Lagoeiro <eduardolagoeiro@yahoo.com.br>
2017-11-30 11:39:54 -0200

    comentários

--- src/main/Main.java
+++ src/main/Main.java
@@ -24,7 +24,9 @@ public class Main {
 			System.out.println("Por favor, entre com um ip na forma a.b.c.d/x.");
 			try {
 				String abcdx = scan.next();
+				//valida entrada a.b.c.d/x decimal e tam de subrede ou mascara e devolve obj ipv4
 				ipv4 = validaEntrada(abcdx);
+			//trata casos com mensages para cada tipo de entrada errada
 			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 			} catch (NumberFormatException e) {
@@ -36,15 +38,19 @@ public class Main {
 
 		switch (menu) {
 		case 1:
+			//imprime informações
 			ipv4.print();
 			break;
 		case 2:
 			boolean b2 = false;
 			do {
+				//le o tamanho do prefixo ou mascara de subrede convertida em tamanho de prefixo
 				int prefixoSubRede = readPrefixoSubRede("\nEntre com o tamanho do prefixo das subredes ou máscara de sub-rede\n->", 0, 30);
 				try {
+					//tenta criar as subNets de acordo com o prefixo de subRede e devolve true caso consiga
 					b2 = ipv4.generateAndPrintSubNet(prefixoSubRede);
 				} catch (IllegalArgumentException e) {
+					//mensagem de erro e limpa buffer
 					System.out.println(e.getMessage());
 					scan.nextLine();
 				}
@@ -57,19 +63,21 @@ public class Main {
 			int inteiro = -1;
 			Integer maxSubNets = ipv4.getMaxSubNets();
 			int numDeSubRedes = 1;
-			boolean first = true;
 			do {
+				//limite superior de maior sub rede possível que o usuário pde pedir
 				int limiteSuperior;
-				if(first){
+				//caso maxSubNets+2 seja uma potencia de 2 o limite é o próprio max
+				if(IPv4.menorPotenciaMaiorOuIgual(maxSubNets) == maxSubNets+2)
 					limiteSuperior = maxSubNets;
-					first = false;
-				}else {
-					if(IPv4.menorPotenciaMaiorOuIgual(maxSubNets) == maxSubNets+2)
-						limiteSuperior = maxSubNets;
-					else{
-						limiteSuperior = IPv4.menorPotenciaMaiorOuIgual(maxSubNets)/2 - 2;
-					}
+				else{
+				//caso não seja ele é a metade da menor potencia maior ou igual a maxSubNets -2 (reservados para end de broad e de subrede)
+				//exemplo, se começamos com 254 endereços validos e foi requerido 2 endereços (4), temos agora 250, porém o maior num que pode ser pedido
+				//a menor potencia maior ou igual a 250 é 254, logo só podemos dar 128 para uma unica rede para não dar overflow e sairmos da nossa subrede
+				//quando o maxSubNet + 2 é uma potencia de 2 isso não é vdd, ele é o próprio limite superior
+				//basicamente queremos a maior potencia de 2 menor ou igual a max subnets, porém para não implementar outra função usei a divisão para facilitar
+					limiteSuperior = IPv4.menorPotenciaMaiorOuIgual(maxSubNets)/2 - 2;
 				}
+				//caso quando as redes estão acabando para não bugar e calcular num negativo o limite superior tem que ser maxSubNets
 				if(limiteSuperior <= 0)
 					limiteSuperior = maxSubNets;
 				System.out.println("\nVocê possui "+maxSubNets+" endereços atribuíveis a interfaces e o maior endereço possível é "
@@ -79,13 +87,20 @@ public class Main {
 				String complemento = "Lembrando que sua rede tem um total de " + maxSubNets
 						+ " interfaces atribuíveis disponíveis e o maior endereço possível é " +
 					limiteSuperior + ".";
+				//lê inteiro entre 0 e a maior subrede possivel
 				inteiro = readPositiveInt(message, 0, limiteSuperior, complemento);
+				//calcula a partir do inteiro + 2 (broadcast e endereço de rede) a menor potencia de 2 maior ou igual o numero
 				int menorPotenciaMaiorOuIgual = IPv4.menorPotenciaMaiorOuIgual(inteiro+2);
+				//se inteiro for 0 não adicionamos ele na lista
 				if (inteiro != 0) {
+					//coloamos a menor potencia de dois maior ou igual ao inteiro na lista
 					inteiros.add(menorPotenciaMaiorOuIgual);
+					//descontamos do valor anterior de max interfaces disponiveis
 					maxSubNets -= menorPotenciaMaiorOuIgual;
+					//incrementamos o numero de subredes que o usuário pediu
 					numDeSubRedes++;
 				}
+				//se o numero max de subnets for 0 paramos o loop pois não temos mais interfaces disponíveis
 				if(maxSubNets <= 0)
 					break;
 			} while (inteiro != 0);
@@ -98,29 +113,40 @@ public class Main {
 		}
 	}
 
+	//lê prefixo de sub rede como valor numerico ou mascara de subrede
 	protected static int readPrefixoSubRede(String string, int min, int max){
 		int entrada = -1;
 		int valor = 0;
 		do{
 			System.out.println(string);
+			//lê string
 			String stringLida = scan.nextLine().trim();
 			try{
+				//tenta parsear o valor entrado como um tamanho de prefixo
 				entrada = Integer.valueOf(stringLida);
 			} catch (NumberFormatException e){
+				//caso em que é uma subnet
 				String[] subnetMask = stringLida.split("\\.");
 				if(subnetMask.length != 4 && stringLida.contains("."))
+					//caso em que a subNet foi escrita de forma inválida
 					throw new IllegalArgumentException("Máscara de sub-rede "+stringLida+" não é válida.");
 				else if(validaMask(stringLida)){
+					//se ela for validada, criamos um obj GenericByte
 					GenericByte gb = new GenericByte(stringLida);
+					//parseamos pra binário
 					String bin = gb.toBinary();
+					//usamos métodos da classe String pra ver quantos 0 tem em sequência
 					String sufix = bin.substring(bin.indexOf("0"));
+					//calculamos o tamanho do prefixo pela quantidade de 0
 					valor = 32 - sufix.length();
 					if (valor > 30 || valor < 0)
+						//caso de tamanho de prefixo é maior que 8 bits
 						throw new IllegalArgumentException("Tamanho de prefixo de sub-rede deve ser entre 0 e 30");
 					else
 						entrada = valor;
 				}
 				else
+					//caso em que os valores não são numéricos
 					throw new NumberFormatException("Os valores de a, b, c, d e x são numericos.");
 			}
 		}while(entrada == -1);
@@ -131,6 +157,7 @@ public class Main {
 		return readPositiveInt(string, min, max, "");
 	}
 
+	//função para leitura de numéros positivos entre min e max e usando um complemento de mensagem para comunicar algo
 	protected static int readPositiveInt(String string, int min, int max, String complemento) {
 		if(max < min) return 0;
 		int menu = -1;
@@ -152,27 +179,38 @@ public class Main {
 		return menu;
 	}
 
+	//função para validar a entrada a.b.c.d/x
 	private static IPv4 validaEntrada(String abcdx) {
 		String[] s = abcdx.split("/");
+		//checa se tem uma parte antes e depois da /
 		if (s.length != 2)
 			throw new InputMismatchException("Você deve entrar com ip no formato a.b.c.d/x.");
 		String abcd = s[0];
 		Integer valueOf = null;
 		try {
+			//tenta parsear o valor entrado como um tamanho de prefixo
 			valueOf = Integer.valueOf(s[1]);
 		} catch (NumberFormatException e) {
+			//caso em que é uma subnet
 			String[] subnetMask = s[1].split("\\.");
 			if(subnetMask.length != 4 && s[1].contains("."))
+				//caso em que a subNet foi escrita de forma inválida
 				throw new IllegalArgumentException("Máscara de sub-rede "+s[1]+" não é válida.");
 			else if(validaMask(s[1])){
+				//se ela for validada, criamos um obj GenericByte
 				GenericByte gb = new GenericByte(s[1]);
+				//parseamos pra binário
 				String bin = gb.toBinary();
+				//usamos métodos da classe String pra ver quantos 0 tem em sequência
 				String sufix = bin.substring(bin.indexOf("0"));
+				//calculamos o tamanho do prefixo pela quantidade de 0
 				valueOf = 32 - sufix.length();
 				if (valueOf > 30 || valueOf < 0)
+					//caso de tamanho de prefixo é maior que 8 bits
 					throw new IllegalArgumentException("Tamanho de prefixo de sub-rede deve ser entre 0 e 30");
 			}
 			else
+				//caso em que os valores não são numéricos
 				throw new NumberFormatException("Os valores de a, b, c, d e x são numericos.");
 		}
 		return new IPv4(abcd, valueOf);
